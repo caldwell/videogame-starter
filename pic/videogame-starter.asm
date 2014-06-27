c4ec59889bc65fed374a8afef410d5b4bace7361
David Caldwell <david@porkrind.org>
2014-06-27 16:21:12 -0700

    Just put GPL license junk in the source code instead of my old include nonsense.

--- pic/videogame-starter.asm
+++ pic/videogame-starter.asm
@@ -1,9 +1,20 @@
-;;; $Header: /cvs/programs/videogame-starter/videogame-starter.asm,v 1.7 2004-09-19 22:25:58 david Exp $
 ;;; -------=====================<<<< COPYRIGHT >>>>========================-------
-;;;          Copyright (c) 2001 David Caldwell,  All Rights Reserved.
-;;;  See full text of copyright notice and limitations of use in file COPYRIGHT.h
-;;; -------================================================================-------
-include "COPYRIGHT.inc"
+;;;   Copyright (c) 2001 David Caldwell <david@porkrind.org>
+
+;;;   This program is free software; you can redistribute it and/or modify
+;;;   it under the terms of the GNU General Public License as published by
+;;;   the Free Software Foundation; either version 2 of the License, or
+;;;   (at your option) any later version.
+
+;;;   This program is distributed in the hope that it will be useful,
+;;;   but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;   GNU General Public License for more details.
+
+;;;   You should have received a copy of the GNU General Public License
+;;;   along with this program; if not, write to the Free Software
+;;;   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; 
 ;;;                           _______  _______
